560cb3c0d899c4b9472ec0d1a048861acbda4e85
shanrathsarana <shanrathsaranadata@gmail.com>
2021-09-28 01:22:54 +0530

    db edit email

--- app/src/main/java/com/coffee_secrets/obj/User.java
+++ app/src/main/java/com/coffee_secrets/obj/User.java
@@ -1,8 +1,11 @@
 package com.coffee_secrets.obj;
 
 import android.net.Uri;
+import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class User {
 
@@ -35,6 +38,19 @@ public class User {
 
 
 
+    }
+
+
+    public static boolean isEditTextContainEmail(EditText argEditText) {
+
+        try {
+            Pattern pattern = Pattern.compile("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+            Matcher matcher = pattern.matcher(argEditText.getText());
+            return matcher.matches();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 
