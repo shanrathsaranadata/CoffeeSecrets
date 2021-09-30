39750150ea5f794ddc6936ffbde01d1812490f79
shanrathsarana <shanrathsaranadata@gmail.com>
2021-09-30 08:09:04 +0530

    spash and user

--- app/src/main/java/com/coffee_secrets/ui/beforehome/Spash.java
+++ app/src/main/java/com/coffee_secrets/ui/beforehome/Spash.java
@@ -1,37 +1,37 @@
 package com.coffee_secrets.ui.beforehome;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.coffee_secrets.R;
 import com.coffee_secrets.obj.DB;
-import com.coffee_secrets.ui.basic.Home_Activity;
-import com.coffee_secrets.ui.basic.MainActivity;
+import com.coffee_secrets.obj.ProgressBarAnimation;
 
 public class Spash extends AppCompatActivity {
 
-    private Button mButton;
+    private ProgressBar progressBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spash);
 
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.progress_bar_tv);
 
-        mButton = findViewById(R.id.da_update);
-        new DB.startUp() {
+
+        DB.startUp start = new DB.startUp() {
             @Override
             public void done() {
-                Intent i = new Intent(Spash.this,
-                        MainActivity.class);
-                startActivity(i);
+                ProgressBarAnimation PBA = new ProgressBarAnimation(Spash.this,progressBar,textView,0f,100f);
+                PBA.setDuration(5000);
+                progressBar.setAnimation(PBA);
             }
         };
-
 
 
 
