9e77130295e6f4a807ae9503b72967ab13ce504d
shanrathsarana <shanrathsaranadata@gmail.com>
2021-09-30 18:39:25 +0530

    all update

---
+++ app/src/main/java/com/coffee_secrets/ui/InquiryActivity.java
@@ -0,0 +1,16 @@
+package com.coffee_secrets.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coffee_secrets.R;
+
+public class InquiryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inquiry);
+    }
+}
